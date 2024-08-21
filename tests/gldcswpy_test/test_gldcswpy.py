e790d4d2b3b1c2428381d8e09ec4b80311fdbc54
KavyaAnnapareddy <78133902+KavyaAnnapareddy@users.noreply.github.com>
2024-08-21 18:38:37 -0400

    initial commit

---
+++ tests/gldcswpy_test/test_gldcswpy.py
@@ -0,0 +1,15 @@
+import numpy as np
+from gldcswpy.gldcswpy import Gld
+
+def test_gldcswpy():
+    # Example data- Chi-squared distribution 
+    np.random.seed(0);
+    data = np.random.chisquare(df=2, size=1000)
+    # initiate class with data
+    g = Gld(data)
+    params = g.get_params(initial_guess=(0.64,0.1991))
+    # VaR at 5%
+    g.VaR(params,0.05)
+    assert np.round(g.VaR(params,0.05),5) == -0.1546
+    
+
